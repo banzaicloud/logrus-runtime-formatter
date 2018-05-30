503f5dec912d73f8c0525c5f21e618d52a25c8c5
Nandor Kracser <bonifaido@gmail.com>
2018-05-30 09:15:29 +0200

    Handle reflected calls

--- formatter.go
+++ formatter.go
@@ -17,6 +17,9 @@ const PackageKey = "package"
 // LineKey holds the line field
 const LineKey = "line"
 
+// FileKey holds the file field
+const FileKey = "file"
+
 // Formatter decorates log entries with function name and package name (optional) and line number (optional)
 type Formatter struct {
 	ChildFormatter logrus.Formatter
@@ -24,11 +27,13 @@ type Formatter struct {
 	Line bool
 	// When true, package name will be tagged to fields as well
 	Package bool
+	// When true, file name will be tagged to fields as well
+	File bool
 }
 
 // Format the current log entry by adding the function name and line number of the caller.
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
-	function, line := f.getCurrentPosition(entry)
+	function, file, line := f.getCurrentPosition(entry)
 
 	packageEnd := strings.LastIndex(function, ".")
 	functionName := function[packageEnd+1:]
@@ -40,28 +45,36 @@ func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if f.Package {
 		data[PackageKey] = function[:packageEnd]
 	}
+	if f.File {
+		data[FileKey] = file
+	}
 	for k, v := range entry.Data {
 		data[k] = v
 	}
 	entry.Data = data
 
-	// entry.Data[FunctionKey] = function
-	// if f.LineNumber {
-	// 	entry.Data[LineKey] = line
-	// }
-
 	return f.ChildFormatter.Format(entry)
 }
 
-func (f *Formatter) getCurrentPosition(entry *logrus.Entry) (string, string) {
+func (f *Formatter) getCurrentPosition(entry *logrus.Entry) (string, string, string) {
 	skip := 6
 	if len(entry.Data) == 0 {
 		skip = 8
 	}
-	function, _, line, _ := runtime.Caller(skip)
+start:
+	pc, file, line, _ := runtime.Caller(skip)
 	lineNumber := ""
 	if f.Line {
 		lineNumber = fmt.Sprintf("%d", line)
 	}
-	return runtime.FuncForPC(function).Name(), lineNumber
+	function := runtime.FuncForPC(pc).Name()
+	if function == "reflect.callMethod" {
+		skip -= 2
+		goto start
+	}
+	if strings.HasPrefix(function, "runtime.call") {
+		skip -= 1
+		goto start
+	}
+	return function, file, lineNumber
 }
